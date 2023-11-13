6ec746858af93d07975bf3c489931ed82b7d8f2b
Fabian Eberhardt <f.eberhardt@htlstp.at>
2023-11-13 19:37:55 +0100

    added 'chess-board'

---
+++ workspace/java-fx/chess-board/src/ChessBoard.java
@@ -0,0 +1,46 @@
+package app;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class ChessBoard extends Application {
+    @Override
+    public void start(Stage stage) {
+        GridPane root = new GridPane();
+
+        int size = 8;
+        Rectangle rect;
+
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                rect = new Rectangle();
+
+                Rectangle eventRect = rect;
+
+                rect.setOnMouseClicked(event -> {
+                    if (eventRect.getFill() == Color.RED) {
+                        int rectRow = GridPane.getRowIndex(eventRect);
+                        int rectCol = GridPane.getColumnIndex(eventRect);
+                        eventRect.setFill( ((rectRow + rectCol) % 2 == 0) ? Color.GRAY : Color.WHITE );
+                    }
+                    else
+                        eventRect.setFill(Color.RED);
+                });
+                rect.setFill( ((row + col) % 2 == 0) ? Color.GRAY : Color.WHITE );
+
+                root.add(rect, col, row);
+                rect.widthProperty().bind(root.widthProperty().divide(size));
+                rect.heightProperty().bind(root.heightProperty().divide(size));
+            }
+        }
+
+        stage.setScene(new Scene(root,400,400));
+        stage.setTitle("ChessApp");
+        stage.show();
+    }
+
+}
---
+++ workspace/java-fx/chess-board/src/ChessBoardLauncher.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+
+public class ChessBoardLauncher {
+
+    public static void main(String[] args) {
+        Application.launch(app.ChessBoard.class, args);
+    }
+
+}
