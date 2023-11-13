8140bd54644e8ad5065ca499afae7f3d1b9bbdcb
Fabian Eberhardt <f.eberhardt@htlstp.at>
2023-11-13 19:30:09 +0100

    added the 'text-combiner'

---
+++ workspace/java-fx/text-combiner/src/TextCombinerLauncher.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+
+public class TextCombinerLauncher {
+
+    public static void main(String[] args) {
+        Application.launch(TextCombiner.class, args);
+    }
+
+}
